bb4cdf0e6b837bdec175e06ee281a481c1c779f5
Nipuni Siriwardhana <157195014+nsiriwardhana@users.noreply.github.com>
2024-08-06 22:57:04 +0530

    Add files via upload
    add

---
+++ AI Blog Assistant/apikey.py
@@ -0,0 +1,2 @@
+open_api_key ="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"
+google_gemini_api_key ="AIzaSyDPy_wC4gZJp8l6A3tnJp23kzPzSIXWcmQ"
